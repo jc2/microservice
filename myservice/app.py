4f6e1b32512221061bdab531adde3d63a2e1094a
jc2 <juancamiloceron@gmail.com>
2018-08-20 21:58:58 -0500

    some tests of the cap 2

--- myservice/app.py
+++ myservice/app.py
@@ -1,9 +1,33 @@
 import os
 from flakon import create_app
-from myservice.views import blueprints
+from flask import Flask
+from konfig import Config
 
+from myservice.views import blueprints
+from myservice.views.home import DummyConverter
 
 _HERE = os.path.dirname(__file__)
-_SETTINGS = os.path.join(_HERE, 'settings.ini')
+settings = os.path.join(_HERE, 'settings.ini')
 
-app = create_app(blueprints=blueprints, settings=_SETTINGS)
+# app = create_app(blueprints=blueprints, settings=_SETTINGS)
+
+
+app = Flask(__name__)
+
+# load configuration
+settings = os.environ.get('FLASK_SETTINGS', settings)
+if settings is not None:
+    app.config_file = Config(settings)
+    app.config.update(app.config_file.get_map('flask'))
+
+app.url_map.converters.update({'is_even': DummyConverter})
+
+# register blueprints
+if blueprints is not None:
+    for bp in blueprints:
+        app.register_blueprint(bp)
+
+
+
+
+
--- myservice/views/home.py
+++ myservice/views/home.py
@@ -1,13 +1,71 @@
 from flakon import JsonBlueprint
+from flask import jsonify, request, Blueprint, url_for, g, request_finished
+
+# home = JsonBlueprint('home', __name__)
+from werkzeug.exceptions import Unauthorized, NotAcceptable
+from werkzeug.routing import BaseConverter, ValidationError
+
+# def add_app_url_map_converter(self, func, name=None):
+#     def register_converter(state):
+#         state.app.url_map.converters[name or func.__name__] = func
+#
+#     self.record_once(register_converter)
+#
+# # monkey-patch the Blueprint object to allow addition of URL map converters
+# Blueprint.add_app_url_map_converter = add_app_url_map_converter
+
+home = Blueprint('home', __name__)
 
 
-home = JsonBlueprint('home', __name__)
+@home.before_request
+def authenticate():
+    if request.authorization:
+        if (request.authorization['username'], request.authorization['password']) == ('admin', 'admin'):
+            g.user = request.authorization['username']
+        else:
+            raise Unauthorized()
+    else:
+        g.user = 'Anonymous'
 
+def finished(sender, response, **extra):
+    print('Finishing request')
 
+request_finished.connect(finished)
+
+# @home.route('/' methods=['POST', 'DELETE', 'GET'])
 @home.route('/')
 def index():
-    """Home view.
+    user = g.user
+    return f"{{'message': 'Home View', 'user': {user} }}", 202, {'Content-Type': 'application/json'}
 
-    This view will return an empty JSON mapping.
-    """
-    return {}
+@home.route('/converter/<int:my_int>')
+def converter(my_int):
+    return jsonify({
+        'message': 'Converter',
+        'my_int': my_int
+    })
+
+class DummyConverter(BaseConverter):
+    def to_python(self, value):
+        if value.isdigit() and int(value) % 2 == 0:
+            return value
+        else:
+            # raise ValidationError()
+            raise NotAcceptable()
+    def to_url(self, value):
+        return 'empty_number' # just to use this option...
+
+
+
+
+# home.add_app_url_map_converter(DummyConverter, 'is_even')
+
+@home.route('/custom_converter')
+@home.route('/custom_converter/<is_even:my_int>')
+def custom_converter(my_int=None):
+    return jsonify({
+        'message': 'Converter',
+        'my_int': my_int if my_int else url_for('home.custom_converter', my_int='whatever')
+    })
+
+
